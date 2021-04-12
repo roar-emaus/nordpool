5d23dc943c8a240fa2029db1f193ff922b05bcbe
Roar Emaus <roar@emaus.no>
2021-04-12 10:12:49 +0200

    ll

--- historisk.py
+++ historisk.py
@@ -1,29 +1,31 @@
 import csv
+#import plotly
+#plotly.io.orca.config.executable = '/home/roar/projects/nordpool/__pypackages__/3.9/bin/orca'
 import plotly.graph_objects as go
 
 
 time_series = dict()
 years = list(range(2021, 2022))
 for year in years:
-    with open(f"data/elspot/{year}.csv", "r") as csvfile:
+    with open(f'data/elspot/{year}.csv', 'r') as csvfile:
         new_values = []
         market_prices = csv.reader(csvfile)
         # Finding headers
         for row in market_prices:
-            if row[1] == "Hours":
+            if row[1] == 'Hours':
                 headers = row[2:]
                 break
         for h in headers:
             if h not in time_series:
                 time_series[h] = [[], []]
         for j, row in enumerate(market_prices):
-            date_str = row[0].split("-")
-            date = f"{date_str[2]}-{date_str[1]}-{date_str[0]}"
+            date_str = row[0].split('-')
+            date = f'{date_str[2]}-{date_str[1]}-{date_str[0]}'
             hour = row[1][:2]
             for i, h in enumerate(headers):
                 v = row[i + 2]
-                value = float(v.replace(",", ".")) if v else float("nan")
-                time_series[h][0].append(f"{date}T{hour}")
+                value = float(v.replace(',', '.')) if v else float('nan')
+                time_series[h][0].append(f'{date}T{hour}')
                 time_series[h][1].append(value)
 
 
@@ -32,10 +34,11 @@ areas = ['Oslo', 'Molde']
 for area in areas:
     area_data = time_series[area]
     fig.add_trace(
-        go.Scatter(x=area_data[0], y=area_data[1], mode="lines", name=area)
+        go.Scatter(x=area_data[0], y=area_data[1], mode='lines', name=area)
     )
 fig.update_traces(line_shape='hv', selector=dict(type='scatter'))
 fig.update_layout(
-    title="EL spot priser", xaxis_title="CEST", yaxis_title="NOK/MWh"
+    title='EL spot priser', xaxis_title='CEST', yaxis_title='NOK/MWh'
 )
-fig.write_html("historisk.html")
+fig.write_html('historisk.html')
+#fig.write_image('historisk.png', width=1920, height=1080)
--- morgendagens.py
+++ morgendagens.py
@@ -1,6 +1,6 @@
 import json
-import plotly
-plotly.io.orca.config.executable = '/home/roar/projects/nordpool/__pypackages__/3.9/bin/orca'
+#import plotly
+#plotly.io.orca.config.executable = '/home/roar/projects/nordpool/__pypackages__/3.9/bin/orca'
 import plotly.graph_objects as go
 
 
@@ -23,11 +23,11 @@ areas = ['Oslo', 'Molde']
 for area in areas:
     area_data = time_series[area]
     fig.add_trace(
-        go.Scatter(x=area_data[0], y=area_data[1], mode="lines+markers",  name=area)
+        go.Scatter(x=area_data[0], y=area_data[1], mode='lines+markers',  name=area)
     )
 fig.update_traces(line_shape='hv', selector=dict(type='scatter'))
 fig.update_layout(
-    title="Morgendages ELSpot priser", xaxis_title="CEST", yaxis_title="NOK/MWh"
+    title='Morgendages ELSpot priser', xaxis_title='CEST', yaxis_title='NOK/MWh'
 )
-fig.write_html("morgendagens.html")
-fig.write_image('morgendagens.png', width=1920, height=1080)
+fig.write_html('morgendagens.html')
+#fig.write_image('morgendagens.png', width=1920, height=1080)
