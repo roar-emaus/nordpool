764d4ee3caa245622f89c8fb51ad679d85a69a67
Roar Emaus <roar@emaus.no>
2021-04-14 12:35:56 +0200

    used Black, put option to make a png

--- morgendagens.py
+++ morgendagens.py
@@ -1,33 +1,39 @@
 import json
-#import plotly
-#plotly.io.orca.config.executable = '/home/roar/projects/nordpool/__pypackages__/3.9/bin/orca'
+
 import plotly.graph_objects as go
 
 
-with open('data/elspot/day_price.html') as f:
+with open("data/elspot/day_price.html") as f:
     data = json.load(f)
-    rows = data['data']['Rows']
-    time_series = {col['Name']: [[], []] for col in rows[0]['Columns']}
+    rows = data["data"]["Rows"]
+    time_series = {col["Name"]: [[], []] for col in rows[0]["Columns"]}
     for row in rows:
-        t = row['StartTime']
-        if row['IsExtraRow']:
+        t = row["StartTime"]
+        if row["IsExtraRow"]:
             break
-        columns = row['Columns']
+        columns = row["Columns"]
         for col in columns:
-            time_series[col['Name']][0].append(t)
-            time_series[col['Name']][1].append(float(col['Value'].replace(',','.')))
+            time_series[col["Name"]][0].append(t)
+            time_series[col["Name"]][1].append(
+                float(col["Value"].replace(" ", "").replace(",", "."))
+            )
 
 
 fig = go.Figure()
-areas = ['Oslo', 'Molde']
+areas = ["Oslo", "Molde"]
 for area in areas:
     area_data = time_series[area]
     fig.add_trace(
-        go.Scatter(x=area_data[0], y=area_data[1], mode='lines+markers',  name=area)
+        go.Scatter(x=area_data[0], y=area_data[1], mode="lines+markers", name=area)
     )
-fig.update_traces(line_shape='hv', selector=dict(type='scatter'))
+fig.update_traces(line_shape="hv", selector=dict(type="scatter"))
 fig.update_layout(
-    title='Morgendages ELSpot priser', xaxis_title='CEST', yaxis_title='NOK/MWh'
+    title="Morgendages ELSpot priser", xaxis_title="CEST", yaxis_title="NOK/MWh"
 )
 fig.write_html("morgendagens.html")
-#fig.write_image('morgendagens.png', width=1920, height=1080)
+
+
+if False:
+    import plotly
+    plotly.io.orca.config.executable = '/home/roar/projects/nordpool/__pypackages__/3.9/bin/orca'
+    fig.write_image('morgendagens.png', width=1920, height=1080)
